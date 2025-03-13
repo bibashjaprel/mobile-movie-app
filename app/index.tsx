145947af1b2e9833c600f626d711dc1ebe489a90
Bibash Japrel <bibashjaprel111@gmail.com>
2025-03-13 09:15:55 +0545

    hello world...

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-primary text-bold font-bold">Welcome!</Text>
+      <Text className="text-5xl text-blue-800 text-bold font-bold">Hello World!</Text>
     </View>
   );
 }
